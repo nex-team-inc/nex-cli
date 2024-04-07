dd0aa606316af85c266f8ed08f2d95dd40aa9b52
Tony Sung <tonysung@users.noreply.github.com>
2024-04-07 22:34:58 +0800

    fix(apksigner): revived cert management using google-cloud-kms package

--- src/nexcli/olympia/apksigner.py
+++ src/nexcli/olympia/apksigner.py
@@ -1,3 +1,4 @@
+import shutil
 import click
 import os.path
 import sys
@@ -7,11 +8,16 @@ import string
 from google.cloud import kms
 from pyaxmlparser import APK
 from rich.console import Console
-from rich.table import Table
 from rich.live import Live
+from rich.table import Table
 from nexcli.utils.locate import find_android_build_tools
 
 CERT_VALIDITY=9125
+
+GCP_PROJECT="playos-signer"
+GCP_KMS_LOCATION = "global"
+GCP_KMS_KEYRING = "playos-app-signer"
+
 INTERNAL_SIGNER = "playos-apk-signer-1"
 EXTERNAL_SIGNER = "playos-apk-signer-2"
 PACKAGE_SIGNERS = {
@@ -44,32 +50,9 @@ PACKAGE_SIGNERS = {
 }
 
 @click.group('apksigner')
-@click.option("--project", help="The Google Cloud project ID to use for KMS operations.", default="playos-signer")
-@click.option("--name", help="Name of the key. Unless otherwise specified, the name of the keystore file, and KMS key are derived from this value.")
-@click.option("--kms-key", help="KMS key ID to use for encryption and decryption.")
-@click.option("--kms-keyring", help="KMS key ring of the key.", default="playos-app-signer")
-@click.option("--kms-location", help="KMS location of the key ring.", default="global")
-@click.option("--keystore-path", help="Path of the keystore file. Paths are relative to the \"secrets\" directory.")
-@click.option("--keystore-password", help="Password for the keystore. If omitted, uses KMS to encrypt and decrypt the keystore password.")
-@click.option("--key-alias", help="Name of the signing key. If omitted, the name without extension of the keystore file will be used.")
-def cli(project, name, kms_key, kms_keyring, kms_location, keystore_path, keystore_password, key_alias):
-    """Nex Platform APK signing utility"""
-    global PROJECT, NAME, KMS_KEY, KEY_ALIAS, KEYSTORE_PATH, KEYSTORE_PASSWORD, KMS_LOCATION, KMS_KEYRING
-
-    PROJECT = project
-    NAME = name
-    KMS_KEY = kms_key
-    KEY_ALIAS = key_alias
-    KEYSTORE_PATH = keystore_path
-    KEYSTORE_PASSWORD = keystore_password
-    KMS_KEYRING = kms_keyring
-    KMS_LOCATION = kms_location
-
-def find_keytool():
-    try:
-        subprocess.run(['keytool'], check=True, capture_output=True)
-    except subprocess.CalledProcessError:
-        sys.exit("error: keytool or JDK could not be found")
+def cli():
+    """APK signing commands."""
+    pass
 
 def run_apksigner(*args, input=None):
     apksigner = os.path.join(find_android_build_tools(), 'apksigner')
@@ -79,8 +62,11 @@ def run_apksigner(*args, input=None):
         input = input.encode('utf-8')
     return subprocess.run(run_args, check=True, capture_output=True, input=input)
 
-def run_keytool(args, capture_output=True):
-    run_args = ['keytool']
+def run_keytool(*args, capture_output=True):
+    executable = 'keytool'
+    if shutil.which(executable) is None:
+        raise click.ClickException(f"{executable} not found in PATH. Please setup JDK.")
+    run_args = [executable]
     run_args.extend(args)
     return subprocess.run(run_args, check=True, capture_output=capture_output)
 
@@ -88,48 +74,29 @@ def generate_password():
     alphabet = string.ascii_letters + string.digits
     return ''.join(secrets.choice(alphabet) for i in range(24))
 
-def get_keystore_password_path(name, keystore_path):
-    keystore_path = get_keystore_path(name, keystore_path)
-    (root, _) = os.path.splitext(keystore_path)
-    return root + '_password.enc'
+def get_keystore_path(name, check=True):
+    keystore_path = os.path.realpath(os.path.join(os.path.dirname(__file__), 'secrets', name + '.jks'))
+    if check and not os.path.isfile(keystore_path):
+        raise click.ClickException(f"Keystore file not found: {keystore_path}")
+    return keystore_path
 
-def get_keystore_path(name = None, keystore_path = None):
-    if keystore_path:
-        return os.path.realpath(os.path.expanduser(keystore_path))
-    if not name:
-        sys.exit("error: argument --name or --keystore-path must be specified.")
-    return os.path.realpath(os.path.join(os.path.dirname(__file__), 'secrets', name + '.jks'))
+def get_keystore_password_path(name, check=True):
+    keystore_path = get_keystore_path(name, check)
+    keystore_password_path = os.path.splitext(keystore_path)[0] + '_password.enc'
+    if check and not os.path.isfile(keystore_password_path):
+        raise click.ClickException(f"Keystore password file not found: {keystore_password_path}")
+    return (keystore_path, keystore_password_path)
 
-def get_kms_key(name, kms_key):
-    if kms_key:
-        return kms_key
-    if not name:
-        sys.exit("error: argument --name or --kms-key must be specified.")
-    return name
-
-def get_key_alias(name, key_alias):
-    if key_alias:
-        return key_alias
-    if not name:
-        sys.exit("error: argument --name or --key-alias must be specified.")
-    return name
-
-def gcloud_decrypt_keystore_password(name, kms_key, keystore_path):
-    keystore_password_path = get_keystore_password_path(name, keystore_path)
-    kms_key = get_kms_key(name, kms_key)
+def get_keystore_password(name):
+    _, keystore_password_path = get_keystore_password_path(name)
     client = kms.KeyManagementServiceClient()
-    key_name = client.crypto_key_path(PROJECT, KMS_LOCATION, KMS_KEYRING, kms_key)
+    key_name = client.crypto_key_path(GCP_PROJECT, GCP_KMS_LOCATION, GCP_KMS_KEYRING, name)
     with open(keystore_password_path, 'rb') as file:
         ciphertext = file.read()
     return client.decrypt(request={
         'name': key_name,
         'ciphertext': ciphertext
     }).plaintext
-
-def get_keystore_password(name = None, kms_key = None, keystore_path = None, keystore_password = None):
-    if keystore_password:
-        return keystore_password
-    return gcloud_decrypt_keystore_password(name, kms_key, keystore_path)
 
 @click.command
 @click.argument("apk", type=click.Path(exists=True))
@@ -163,7 +130,7 @@ def sign(apk, output=None, signer=None):
 
         table.add_row("Input File", apk)
         table.add_row("Package Name", metadata.package)
-        table.add_row("Version Code", metadata.version_code)        
+        table.add_row("Version Code", metadata.version_code)
         live.refresh()
 
         # Verify the current signature
@@ -177,6 +144,7 @@ def sign(apk, output=None, signer=None):
         if output is None:
             output = os.path.splitext(apk)[0] + '-signed' + os.path.splitext(apk)[1]
         table.add_row("Output File", output)
+        table.add_row("New Signer", signer)
         live.refresh()
 
         # Sign the APK
@@ -198,123 +166,109 @@ def sign(apk, output=None, signer=None):
             table.add_row("New Signature", result.stdout.decode('utf-8').strip())
             live.refresh()
         except subprocess.CalledProcessError as e:
-            click.echo(f'Cannot verify new signature: {e.stderr.decode("utf-8")}', err=True)        
+            click.echo(f'Cannot verify new signature: {e.stderr.decode("utf-8")}', err=True)
 
 @click.command
-def print_cert():
-    """Print information about the signing certificate in the keystore file"""
+@click.argument("signer")
+def print_cert(signer):
+    """Print a signing certificate"""
 
-    find_keytool()
+    keystore_path = get_keystore_path(signer)
+    keystore_password = get_keystore_password(signer)
 
-    keystore_path = get_keystore_path(NAME, KEYSTORE_PATH)
-    keystore_password = get_keystore_password(NAME, KMS_KEY, KEYSTORE_PATH, KEYSTORE_PASSWORD)
-
-    if not os.path.isfile(keystore_path):
-        sys.exit(f"error: keystore file not found: {keystore_path}")
+    click.echo(f"...listing keystore entries at {keystore_path}")
 
     try:
-        print(f"... listing keystore entries ({keystore_path})")
-        run_keytool(['-list',
-                     '-v',
-                     '-keystore', keystore_path,
-                     '-storepass', keystore_password], capture_output=False)
+        run_keytool('-list',
+                    '-v',
+                    '-keystore', keystore_path,
+                    '-storepass', keystore_password, capture_output=False)
     except subprocess.CalledProcessError as e:
-        print(e.stderr.decode('utf-8'))
-        sys.exit('error: failed to create new signing certificate')
+        raise click.ClickException(f"Failed to list keystore entries: {e.stderr.decode('utf-8')}")
 
 @click.command
-@click.argument("file")
-def export_cert(file):
-    """Export the signing certificate in the keystore file"""
+@click.argument("signer")
+@click.option("-o", "--output", help="Path to the output file.", required=True)
+def export_cert(signer, output):
+    """Export a signing certificate to a file"""
 
-    find_keytool()
+    keystore_path = get_keystore_path(signer)
+    keystore_password = get_keystore_password(signer)
 
-    keystore_path = get_keystore_path(NAME, KEYSTORE_PATH)
-    keystore_password = get_keystore_password(NAME, KMS_KEY, KEYSTORE_PATH, KEYSTORE_PASSWORD)
-    key_alias = get_key_alias()
-
-    if not os.path.isfile(keystore_path):
-        sys.exit(f"error: keystore file not found: {keystore_path}")
+    click.echo(f"... exporting signing certificate from keystore {keystore_path}")
 
     try:
-        print(f"... exporting signing certificate from keystore {keystore_path}")
-        run_keytool(['-exportcert',
-                     '-file', file,
-                     '-alias', key_alias,
-                     '-keystore', keystore_path,
-                     '-storepass', keystore_password], capture_output=False)
+        run_keytool('-exportcert',
+                    '-file', output,
+                    '-alias', signer,
+                    '-keystore', keystore_path,
+                    '-storepass', keystore_password, capture_output=False)
     except subprocess.CalledProcessError as e:
-        print(e.stderr.decode('utf-8'))
-        sys.exit('error: failed to create new signing certificate')
+        raise click.ClickException(f"Failed to export signing certificate: {e.stderr.decode('utf-8')}")
 
 @click.command
+@click.argument("signer")
 @click.option("--dname", help='"distinguished name" to use for creating signing key.')
-def create_cert(dname):
-    """Create new APK signing certificate"""
+def create_cert(signer, dname):
+    """Create a new signing certificate"""
+    click.echo(f"... creating new signing certificate")
 
-    raise click.ClickException("This command is not yet implemented")
-
-    find_keytool()
-
-    if ctx.obj["YES"]:
-        sys.exit("error: argument --yes is not allowed when creating keystore")
-
-    keystore_path = get_keystore_path(NAME, KEYSTORE_PATH)
-    keystore_password_path = get_keystore_password_path(NAME, KEYSTORE_PATH)
-    keystore_password = generate_password()
-    kms_key = get_kms_key()
-    key_alias = get_key_alias()
-
+    keystore_path, keystore_password_path = get_keystore_password_path(signer, check=False)
     if os.path.isfile(keystore_path):
-        sys.exit(f"error: keystore file already exists: {keystore_path}")
+        raise click.ClickException(f"Keystore file already exists: {keystore_path}")
+    
+    console = Console()    
+    table = Table(show_header=False, show_lines=True)
+    table.add_row("Signer", signer)
+    table.add_row("Keystore Path", keystore_path)
+    table.add_row("Keystore Password Path", keystore_password_path)
 
-    try:
-        run_gcloud(['kms', 'keys', 'describe', kms_key,
-                    '--location', ctx.obj["KMS_LOCATION"],
-                    '--keyring', ctx.obj["KMS_KEYRING"]])
-        print(f"... KMS encryption key \"{kms_key}\" is found")
-    except subprocess.CalledProcessError:
-        try:
-            print(f"... creating KMS encryption key \"{kms_key}\"")
-            run_gcloud(['kms', 'keys', 'create', kms_key,
-                        '--location', ctx.obj["KMS_LOCATION"],
-                        '--keyring', ctx.obj["KMS_KEYRING"],
-                        '--purpose', 'encryption'])
-        except subprocess.CalledProcessError as e:
-            print(e.stderr.decode('utf-8'))
-            sys.exit('error: failed to create KMS key')
+    client = kms.KeyManagementServiceClient()
+    key_name = client.crypto_key_path(GCP_PROJECT, GCP_KMS_LOCATION, GCP_KMS_KEYRING, signer)
+    table.add_row("GCP KMS Key", key_name)
+    console.print(table)
 
-    try:
-        print("... saving encrypted keystore password")
-        run_gcloud(['kms', 'encrypt',
-                    '--plaintext-file', '-',
-                    '--ciphertext-file', keystore_password_path,
-                    '--location', ctx.obj["KMS_LOCATION"],
-                    '--keyring', ctx.obj["KMS_KEYRING"],
-                    '--key', kms_key], input=keystore_password)
-    except subprocess.CalledProcessError as e:
-        print(e.stderr.decode('utf-8'))
-        sys.exit('error: failed to encrypt keystore password')
+    key_ring_path = client.key_ring_path(GCP_PROJECT, GCP_KMS_LOCATION, GCP_KMS_KEYRING)
+    crypto_keys = client.list_crypto_keys({'parent': key_ring_path})
+    crypto_key = next((key for key in crypto_keys if key.name == key_name), None)
+    if crypto_key is None:
+        click.echo(f"... creating new GCP KMS key")
+        crypto_key = client.create_crypto_key({
+            'parent': key_ring_path,
+            'crypto_key_id': signer,
+            'crypto_key': {
+                'purpose': kms.CryptoKey.CryptoKeyPurpose.ENCRYPT_DECRYPT
+            },
+        })
+    else:
+        click.echo(f"... using existing GCP KMS key created at {crypto_key.create_time}")
 
+    click.echo(f"... generating and encrypting new keystore password")
+    keystore_password = generate_password()
+    encrypt_response = client.encrypt({'name': key_name, 'plaintext': keystore_password.encode('utf-8')})
+    with open(keystore_password_path, 'wb') as file:
+        file.write(encrypt_response.ciphertext)
+
+    click.echo(f"... creating new signing certificate keystore")
     try:
-        print("... creating new signing certificate keystore")
         genkey_args = ['-genkey',
                        '-keystore', keystore_path,
-                       '-alias', key_alias,
+                       '-alias', signer,
                        '-storepass', keystore_password,
                        '-validity', str(CERT_VALIDITY),
                        '-keysize', '2048',
                        '-keyalg', 'RSA']
         if dname:
             genkey_args.extend(['-dname', dname])
-        run_keytool(genkey_args, capture_output=False)
+        run_keytool(*genkey_args, capture_output=False)
     except subprocess.CalledProcessError as e:
-        print(e.stderr.decode('utf-8'))
-        sys.exit('error: failed to create new signing certificate')
+        raise click.ClickException(f"Failed to create new signing certificate: {e.stderr.decode('utf-8')}")
 
-    if ask_yesno('Print password for backup?', default=True):
-        print("\nKeystore password:\n")
-        print(keystore_password)
+    if click.confirm('Print password for backup?'):
+        table = Table()
+        table.add_column("Keystore Password")
+        table.add_row(keystore_password)
+        console.print(table)
 
 cli.add_command(sign)
 cli.add_command(verify)
