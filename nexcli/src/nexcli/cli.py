ef240aa7343a9afa1fdb6c8f829bc0e07eeb3924
Matthew Chan <matthew@nex.team>
2024-04-16 14:13:57 +0800

    feat: Use entry points to configure main cli.
    * Switch to hatchling for building.

--- nexcli/src/nexcli/cli.py
+++ nexcli/src/nexcli/cli.py
@@ -1,10 +1,28 @@
+import importlib.metadata
+from importlib.metadata import entry_points, distributions, metadata
+import os
+import sys
+
 import click
-from . import adb, drive, olympia
 
 @click.group()
 def main():
+    '''Main driver'''
     pass
 
-main.add_command(adb.cli)
-main.add_command(drive.cli)
-main.add_command(olympia.cli)
+# TODO(matthew_chan): Convert to lazy loading commands.
+@main.group
+def components():
+    pass
+
+discovered = entry_points(group='nexcli.subcommands')
+for plugin in discovered:
+    main.add_command(plugin.load())
+
+@components.command
+def upgrade():
+    installed_plugins = set(
+        plugin.dist.name for plugin in discovered
+    )
+    click.echo(f"Discovered plugin: {' '.join(installed_plugins)}")
+    os.execv(sys.executable, [sys.executable, '-m', 'pip', 'install', '--upgrade', *installed_plugins])
