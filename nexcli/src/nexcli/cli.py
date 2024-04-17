68e122071d27296423cbe9d31065e7205e976fb0
Matthew Chan <matthew@nex.team>
2024-04-17 11:37:40 +0800

    feat: Implement lazy loading of subcommands.
    * Also added better exclude in vscode settings.
    * Enable auto format on save.

--- nexcli/src/nexcli/cli.py
+++ nexcli/src/nexcli/cli.py
@@ -1,30 +1,68 @@
-from importlib.metadata import entry_points, distributions, metadata
+from importlib.metadata import EntryPoints, entry_points, distributions, metadata
+from typing import List
 import os
 import sys
 
 import click
 
-@click.group()
-def main():
-    '''Main driver'''
-    pass
 
-# TODO(matthew_chan): Convert to lazy loading commands.
-@main.group
+class LazySubcommands(click.Group):
+    def __init__(
+        self,
+        *args,
+        subcommand_list: EntryPoints = None,
+        components_group: "click.Group" = None,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.subcommand_list = subcommand_list
+        self.components_group = components_group
+
+    def list_commands(self, ctx: click.Context) -> List[str]:
+        base = super().list_commands(ctx)
+        lazy = [entry_point.name for entry_point in self.subcommand_list]
+        return sorted(base + lazy)
+
+    def get_command(self, ctx: click.Context, cmd_name: str) -> click.Command | None:
+        try:
+            entry_point = self.subcommand_list[cmd_name]
+            return entry_point.load()
+        except KeyError:
+            pass
+        return super().get_command(ctx, cmd_name)
+
+
+@click.group
 def components():
+    "Handle installed components"
     pass
 
-subcommands = entry_points(group='nexcli.subcommands')
-for plugin in subcommands:
-    main.add_command(plugin.load())
+
+subcommands = entry_points(group="nexcli.subcommands")
+
+
+@click.group(
+    cls=LazySubcommands,
+    subcommand_list=subcommands,
+    components_group=components,
+    help="Main Driver",
+)
+def main():
+    pass
+
+
+main.add_command(components)
+
 
 @components.command
 def upgrade():
-    installed_plugins = set(
-        plugin.dist.name for plugin in subcommands
-    )
+    installed_plugins = set(plugin.dist.name for plugin in subcommands)
     click.echo(f"Discovered plugin: {' '.join(installed_plugins)}")
-    os.execv(sys.executable, [sys.executable, '-m', 'pip', 'install', '--upgrade', *installed_plugins])
+    os.execv(
+        sys.executable,
+        [sys.executable, "-m", "pip", "install", "--upgrade", *installed_plugins],
+    )
+
 
 @components.command
 def debug():
