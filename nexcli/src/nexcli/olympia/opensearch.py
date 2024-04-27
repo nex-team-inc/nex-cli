c78111494db12bd423e58204f1d8f7f14a31ddf1
Tony Sung <tonysung@users.noreply.github.com>
2024-04-27 13:02:03 +0800

    feat(nexcli): WIP support for using aws cli auth for opensearch

--- nexcli/src/nexcli/olympia/opensearch.py
+++ nexcli/src/nexcli/olympia/opensearch.py
@@ -1,7 +1,8 @@
-import os
+import boto3
 import click
-from opensearchpy import OpenSearch
+from opensearchpy import OpenSearch, RequestsHttpConnection
 from opensearch_dsl import Search, Q
+from requests_aws4auth import AWS4Auth
 
 @click.group('opensearch')
 def cli():
@@ -10,19 +11,36 @@ def cli():
 
 @click.command()
 @click.argument('device_id')
-@click.option('-p', '--password', required=True, help='Password for OpenSearch.')
+@click.option('-p', '--password', help='Password for OpenSearch.')
 def search(device_id, password):
     """Search for an eng device logs in OpenSearch."""
     host = 'os.dev.poseidon.npg.games'
     port = 443
     index_name = 'android-logs-eng-*'
-    auth = ('nex-support', password)
+
+    if password is None:
+        session = boto3.Session(profile_name="nexcli-olympia-opensearch")
+        credentials = session.get_credentials()
+
+        if credentials is None:
+            raise click.ClickException('No AWS credentials found. Please sign-in with "aws configure sso"')
+
+        auth = AWS4Auth(
+            credentials.access_key,
+            credentials.secret_key,
+            'us-east-2',
+            'es',  # Service code for OpenSearch
+            session_token=credentials.token  # Required if using temporary credentials
+        )
+    else:
+        auth = ('nex-support', password)
 
     # Creating OS client.
     client = OpenSearch(
         hosts=[{'host': host, 'port': port}],
         http_auth=auth,
         use_ssl=True,
+        connection_class=RequestsHttpConnection,
     )
 
     query = Q("match", deviceId=device_id) & Q("range", **{"@timestamp": {"gte": "now-1d", "lte": "now"}})
