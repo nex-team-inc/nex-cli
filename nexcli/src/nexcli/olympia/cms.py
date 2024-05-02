584f72f5d12ae80a32948f07541f591fd855a23c
Tony Sung <tonysung@users.noreply.github.com>
2024-05-02 21:32:11 +0800

    feat(nexcli): show latest release for a package in CMS

--- nexcli/src/nexcli/olympia/cms.py
+++ nexcli/src/nexcli/olympia/cms.py
@@ -9,6 +9,7 @@ from requests_toolbelt.multipart.encoder import (
     MultipartEncoder,
     MultipartEncoderMonitor,
 )
+from rich.console import Console
 from rich.table import Table
 from tqdm import tqdm
 
@@ -178,27 +179,30 @@ def publish(apk, label, is_prod, no_sign):
         click.echo(response.text)
 
 
+# packageName=team.nex.something&environment=default&rolloutGroup=1
 @click.command()
 @click.argument("pkg_name")
 @click.option(
-    "-a", "--all", is_flag=True, help="List all releases, including inactive ones."
+    "-e",
+    "--environment",
+    default="default",
+    help='The release environment (default = "default").',
 )
 @click.option(
-    "-e", "--environment", help="Include only releases in a specific environment."
+    "-g", "--rollout-group", default=1, help="The rollout group (default = 1)."
 )
 @click.option("-p", "--production", "is_prod", is_flag=True)
-def list(pkg_name, all, environment, is_prod):
-    """List releases in CMS."""
+def latest(pkg_name, environment, rollout_group, is_prod):
+    """Show the latest release for a package."""
 
     api_url = cms_api(is_prod)
     api_token = get_token(is_prod)
 
     params = {
-        "sort[0]": "versionCode:desc",
-        "filters[packageName][$eq]": pkg_name,
+        "packageName": pkg_name,
+        "environment": environment,
+        "rolloutGroup": rollout_group,
     }
-    if environment:
-        params["filters[environments][$eq]"] = environment
 
     res = requests.get(
         api_url + "/releases",
@@ -213,28 +217,81 @@ def list(pkg_name, all, environment, is_prod):
         message = res.json().get("error", {}).get("message", res.text)
         raise click.ClickException(f"{res.status_code} {message}")
 
-    data = res.json()
+    data = res.json().get("data", {})
+    release_attrs = data.get("attributes", {})
+    apk_attrs = release_attrs.get("apk", {}).get("data", {}).get("attributes", {})
 
-    if data.meta.pageCount > 1:
-        click.echo(f"... showing first {data.meta.pageSize}")
+    table = Table(show_header=False, show_lines=True)
+    table.add_row("ID", str(data["id"]))
+    table.add_row("Package Name", release_attrs["packageName"])
+    table.add_row("Version Code", str(release_attrs["versionCode"]))
+    table.add_row("URL", apk_attrs["url"])
+    table.add_row("Filename", apk_attrs["name"])
+    table.add_row("Size", f"{apk_attrs["size"] / 1024:.0f} MB")
+    table.add_row("Updated", apk_attrs["updatedAt"])
 
-    table = Table(show_header=True)
-    table.add_column("Package Name")
-    table.add_column("Version Code")
-    table.add_column("Rollout Group")
-    table.add_column("Environment(s)")
-    table.add_column("Notes")
-    for item in data.data:
-        table.add_row(
-            item.attributes.packageName,
-            item.attributes.versionCode,
-            f"{item.attributes.rolloutGroupMin}-{item.attributes.rolloutGroupMax}",
-            ", ".join(item.attributes.environments),
-            item.attributes.notes,
+    console = Console()
+    console.print(table)
+
+
+if False:
+    @click.command()
+    @click.argument("pkg_name")
+    @click.option(
+        "-a", "--all", is_flag=True, help="List all releases, including inactive ones."
+    )
+    @click.option(
+        "-e", "--environment", help="Include only releases in a specific environment."
+    )
+    @click.option("-p", "--production", "is_prod", is_flag=True)
+    def list(pkg_name, all, environment, is_prod):
+        """List releases in CMS."""
+
+        api_url = cms_api(is_prod)
+        api_token = get_token(is_prod)
+
+        params = {
+            "sort[0]": "versionCode:desc",
+            "filters[packageName][$eq]": pkg_name,
+        }
+        if environment:
+            params["filters[environments][$eq]"] = environment
+
+        res = requests.get(
+            api_url + "/releases",
+            params=params,
+            headers={
+                "Authorization": f"Bearer {api_token}",
+            },
         )
-    table.print()
+
+        if res.status_code != 200:
+            # print(res.headers)
+            message = res.json().get("error", {}).get("message", res.text)
+            raise click.ClickException(f"{res.status_code} {message}")
+
+        data = res.json()
+
+        if data["meta"]["pageCount"] > 1:
+            click.echo(f"... showing first {data.meta.pageSize}")
+
+        table = Table(show_header=True)
+        table.add_column("Package Name")
+        table.add_column("Version Code")
+        table.add_column("Rollout Group")
+        table.add_column("Environment(s)")
+        table.add_column("Notes")
+        for item in data["data"]:
+            table.add_row(
+                item.attributes.packageName,
+                item.attributes.versionCode,
+                f"{item.attributes.rolloutGroupMin}-{item.attributes.rolloutGroupMax}",
+                ", ".join(item.attributes.environments),
+                item.attributes.notes,
+            )
+        table.print()
 
 
 cli.add_command(auth)
-cli.add_command(list)
+cli.add_command(latest)
 cli.add_command(publish)
