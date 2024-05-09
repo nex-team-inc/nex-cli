639f6a01be9b265d44971fd00b1d95351a7667a1
Matthew Chan <matthew@nex.team>
2024-05-09 13:02:35 +0800

    Update nexcli to add help in setting up completion.

---
+++ nexcli/src/nexcli/completion/setup.py
@@ -0,0 +1,38 @@
+import click
+from click.shell_completion import get_completion_class, shell_complete
+from ..cli import main as driver
+from pathlib import Path
+import os
+
+CONFIG_PATH = Path.home() / ".nexcli"
+
+@click.command(name="setup-completion")
+@click.option(
+    "-s", "--shell", type=click.Choice(("zsh",), case_sensitive=False), default="zsh"
+)
+def setup(shell: str):
+    """Setup shell completion."""
+    cls = get_completion_class(shell)
+    comp = cls(driver, {}, "nex", "_NEX_COMPLETE")
+    os.makedirs(CONFIG_PATH, exist_ok=True)
+    script_path = CONFIG_PATH / f"nex-complete.{shell}"
+    with open(script_path, "w") as file:
+        file.write(comp.source())
+    # Check if the ~/.zshrc is okay or not.
+    instruction = f". ~/.nexcli/nex-complete.{shell}"
+    rc_path = Path.home() / f".{shell}rc"
+    if not os.path.isfile(rc_path):
+        with open(rc_path, "w") as file:
+            file.write(line)
+    else:
+        ready = False
+        with open(rc_path, "r") as source:
+            for line in source:
+                if line.strip() == instruction:
+                    ready = True
+                    break
+        if not ready:
+            with open(rc_path, "a") as file:
+                file.write("\n")
+                file.write(instruction)
+                file.write("\n")
